c89123abf637cf73f0ee03c044c1a53c68886ca5
Muhammad MI Shafi <111156790+Dark-asteric@users.noreply.github.com>
2024-01-29 16:59:01 +0100

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -77,35 +77,34 @@ int main()
 
         mark=(player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1')
-
+        if (choice == 1 && square[1] == '1'){
             square[1] = mark;
-        else if (choice == 2 && square[2] == '2')
-
+	}
+        else if (choice == 2 && square[2] == '2'){
             square[2] = mark;
-        else if (choice == 3 && square[3] == '3')
-
+	}
+        else if (choice == 3 && square[3] == '3'){
             square[3] = mark;
-        else if (choice == 4 && square[4] == '4')
-
+	}
+        else if (choice == 4 && square[4] == '4'){
             square[4] = mark;
-        else if (choice == 5 && square[5] == '5')
-
+	}
+        else if (choice == 5 && square[5] == '5'){
             square[5] = mark;
-        else if (choice == 6 && square[6] == '6')
-
+	}
+        else if (choice == 6 && square[6] == '6'){
             square[6] = mark;
-        else if (choice == 7 && square[7] == '7')
-
+	}
+        else if (choice == 7 && square[7] == '7'){
             square[7] = mark;
-        else if (choice == 8 && square[8] == '8')
-
-            square[8] = mark;
-        else if (choice == 9 && square[9] == '9')
-
-            square[9] = mark;
-        else
-        {
+	}
+        else if (choice == 8 && square[8] == '8'){
+		square[8] = mark;
+	}
+        else if (choice == 9 && square[9] == '9'){
+		square[9] = mark;
+	}
+        else{
             cout<<"Invalid move ";
 
             player--;
@@ -117,11 +116,11 @@ int main()
         player++;
     }while(i==-1);
     board();
-    if(i==1)
-
-        cout<<"==>\aPlayer "<<--player<<" win ";
+    if(i==1){
+        cout<<"\aPlayer "<<--player<<" win ";
+    }
     else
-        cout<<"==>\aGame draw";
+        cout<<"\aGame draw";
 
     cin.ignore();
     cin.get();
